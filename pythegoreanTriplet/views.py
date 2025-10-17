d8c600b5d60afe3da25a23f04eb9d29826bc53cc
sparshgoyal199 <sparshgoyal199@gmail.com>
2025-10-17 15:22:42 +0530

    variable names changed in pythegoreanTriplet

--- pythegoreanTriplet/views.py
+++ pythegoreanTriplet/views.py
@@ -6,23 +6,23 @@ def lcf(compoundedAmount,principle):
         return compoundedAmount
     return lcf(principle,compoundedAmount % principle)
 def calculate_pythegorean_template_v1(request):
-    AC = 17
-    AB = 8
-    BC = 15
-    diff = AC - BC
-    factor = lcf(AC+AB,BC)
-    numerator = int((AC+AB)/factor)
-    denominator = int(BC/factor)
-    context = {"AC":AC,"AB":AB,"BC":BC,"diff":diff,"factor":factor,"numerator":numerator,"denominator":denominator}
+    hypotenuse = 17
+    perpendicular = 8
+    base = 15
+    hypotenuse_base = hypotenuse - base
+    factor = lcf(hypotenuse+perpendicular,base)
+    numerator = int((hypotenuse+perpendicular)/factor)
+    denominator = int(base/factor)
+    context = {"hypotenuse":hypotenuse,"perpendicular":perpendicular,"base":base,"hypotenuse_base":hypotenuse_base,"factor":factor,"numerator":numerator,"denominator":denominator}
     return render('request','temp1.html',context)
 
 def calculate_pythegorean_template_v2(request):
-    AC = 13
-    AB = 5
-    BC = 12
-    diff = AC - BC
-    factor = lcf(AC+AB,BC)
-    numerator = int((AC+AB)/factor)
-    denominator = int(BC/factor)
-    context = {"AC":AC,"AB":AB,"BC":BC,"diff":diff,"factor":factor,"numerator":numerator,"denominator":denominator}
+    hypotenuse = 13
+    perpendicular = 5
+    base = 12
+    hypotenuse_base = hypotenuse - base
+    factor = lcf(hypotenuse+perpendicular,base)
+    numerator = int((hypotenuse+perpendicular)/factor)
+    denominator = int(base/factor)
+    context = {"hypotenuse":hypotenuse,"perpendicular":perpendicular,"base":base,"hypotenuse_base":hypotenuse_base,"factor":factor,"numerator":numerator,"denominator":denominator}
     return render('request','temp1.html',context)
